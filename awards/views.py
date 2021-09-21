e464537856e203b6d79a0f99294cc9f38e1b213b
franklin <franklinwanami@gmail.com>
2021-09-21 12:27:31 +0300

    initializing

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,9 @@
+from django.http  import HttpResponse
 from django.shortcuts import render
+import datetime as dt
 
 # Create your views here.
+
+
+def welcome(request):
+    return render (request,'welcome.html')
