46d7ebf7c3343cc2803002274bb06e629daced70
franklin <franklinwanami@gmail.com>
2021-09-21 16:02:53 +0300

    created db

--- grant/settings.py
+++ grant/settings.py
@@ -79,6 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'garlinsk',
+        'PASSWORD': 'kuloba',
     }
 }
 
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
