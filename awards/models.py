3710ac68a8fc016174faebb45d082ca34e9fd1ff
franklin <franklinwanami@gmail.com>
2021-09-23 08:52:51 +0300

    added basic form

--- awards/models.py
+++ awards/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime as dt
+from PIL import Image
+
 # Create your models here.
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    photo = models.ImageField(upload_to='profile_pics/', blank=True, default='profile_pics/default.jpg')
+    photo = models.ImageField(
+        upload_to='profile_pics/', blank=True, default='profile_pics/default.jpg')
     about = models.TextField(blank=True)
 
     def save_profile(self):
@@ -21,9 +25,58 @@ class Profile(models.Model):
         self.delete()
 
     def __str__(self):
-        return  self.about
+        return self.about
 
     class Meta:
         verbose_name = 'Profile'
         verbose_name_plural = 'Profiles'
-        
+class Projects(models.Model):
+    project_title = models.CharField(max_length=255)
+    project_image = models.ImageField(
+        upload_to='images/', default='images/default.jpg')
+    project_description = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    Author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    author_profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, blank=True, default='1')
+    link = models.URLField()
+   
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
+    @classmethod
+    def get_projects(cls):
+        projects = cls.objects.all()
+        return projects
+
+    @classmethod
+    def search_projects(cls, search_term):
+        projects = cls.objects.filter(project_title__icontains=search_term)
+        return projects
+
+    @classmethod
+    def get_by_author(cls, Author):
+        projects = cls.objects.filter(Author=Author)
+        return projects
+
+    @classmethod
+    def get_project(request, id):
+        try:
+            project = Projects.objects.get(pk=id)
+
+        except ObjectDoesNotExist:
+            raise Http404()
+
+        return project
+
+    def __str__(self):
+        return self.project_title
+
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'My Project'
+        verbose_name_plural = 'Projects'
--- awards/views.py
+++ awards/views.py
@@ -1,9 +1,25 @@
 from django.http  import HttpResponse
 from django.shortcuts import render
 import datetime as dt
-
+from .models import *
 # Create your views here.
 
 
 def index(request):
-    return render (request,'index.html')
+    date = dt.date.today()
+    projects = Projects.get_projects()
+
+    return render(request, 'index.html', {"date": date, "projects": projects})
+
+
+def search_projects(request):
+    if 'keyword' in request.GET and request.GET["keyword"]:
+        search_term = request.GET.get("keyword")
+        searched_projects = Projects.search_projects(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html', {"message": message, "projects": searched_projects})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message": message})
