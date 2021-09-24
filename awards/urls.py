62137478f2f258698a04ef7c15a4523c13d295bd
franklin <franklinwanami@gmail.com>
2021-09-25 01:22:29 +0300

    ratings

--- awards/urls.py
+++ awards/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
     url(r'^accounts/profile/$', views.user_profiles, name='profile'),
     url(r'^api/projects/$', views.ProjectList.as_view()),
     url(r'^api/profiles/$', views.ProfileList.as_view()),
+    url(r'^ratings/', include('star_ratings.urls',
+        namespace='ratings')),
 
 ]
 
--- grant/settings.py
+++ grant/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'tinymce',
     'rest_framework',
     'rest_framework.authtoken',
+    'star_ratings',
 
 ]
 
